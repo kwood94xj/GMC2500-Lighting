c2bf285ee1e972b7ffc87e6ba0295861d00a3e1c
kwood94xj <kwood94xj@gmail.com>
2026-03-10 13:15:58 -0400

    Add CanManager class with begin and poll methods

--- include/can_manager.h
+++ include/can_manager.h
@@ -1,1 +1,11 @@
+#pragma once
+#include "driver/twai.h"
+#include "app_state.h"
 
+class CanManager {
+public:
+  bool begin(uint8_t txPin, uint8_t rxPin);
+  void poll(VehicleRequests& req);
+private:
+  void decode(const twai_message_t& msg, VehicleRequests& req);
+};
