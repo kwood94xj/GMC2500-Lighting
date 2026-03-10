cf34e5c5ec0b4dcd268bf33e82acd53031496f01
kwood94xj <kwood94xj@gmail.com>
2026-03-10 13:16:38 -0400

    Add SensorManager class for sensor management

--- include/sensor_manager.h
+++ include/sensor_manager.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <Adafruit_INA226.h>
+#include "app_state.h"
+#include "app_config.h"
 
+class SensorManager {
+public:
+  bool begin();
+  void poll(Telemetry& telem);
+private:
+  Adafruit_INA226 ina226_;
+  float readNodeVolts(uint8_t pin) const;
+  bool readIgnitionState() const;
+  float readTmp36C() const;
+};
