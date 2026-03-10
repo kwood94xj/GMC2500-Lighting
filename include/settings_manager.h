5ead4bccc10733f8479f768fdce76993318b6039
kwood94xj <kwood94xj@gmail.com>
2026-03-10 13:16:53 -0400

    Create SettingsManager class for settings management
    Added SettingsManager class for managing application settings.

--- include/settings_manager.h
+++ include/settings_manager.h
@@ -1,1 +1,21 @@
+#pragma once
+#include <Preferences.h>
+#include "app_config.h"
+#include "app_state.h"
 
+class SettingsManager {
+public:
+  bool begin();
+  void load(StripRuntime* strips,
+            app::RoleMapCfg& roleCfg,
+            app::GlobalLightingCfg& lightingCfg,
+            app::ProtectionCfg& protectionCfg);
+
+  void saveRoleMap(const app::RoleMapCfg& roleCfg);
+  void saveLighting(const app::GlobalLightingCfg& lightingCfg);
+  void saveProtection(const app::ProtectionCfg& protectionCfg);
+  void saveStrip(uint8_t id, const app::StripConfig& cfg);
+  void saveAllStrips(StripRuntime* strips);
+private:
+  Preferences prefs_;
+};
