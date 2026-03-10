90ae466ec74c39c3ded706d69fd73ca33a3d2c98
kwood94xj <kwood94xj@gmail.com>
2026-03-10 13:13:44 -0400

    Add helper functions for bit manipulation and color conversion

--- include/helpers.h
+++ include/helpers.h
@@ -1,1 +1,7 @@
+#pragma once
+#include <FastLED.h>
+#include "app_config.h"
 
+inline uint8_t bitMask(uint8_t i) { return static_cast<uint8_t>(1u << i); }
+inline bool inMask(uint8_t strip, uint8_t mask) { return (mask & bitMask(strip)) != 0; }
+inline CRGB toCRGB(const app::RGB& c) { return CRGB(c.r, c.g, c.b); }
